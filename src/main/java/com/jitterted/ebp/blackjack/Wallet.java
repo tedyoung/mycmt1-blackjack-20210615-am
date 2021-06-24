9f952cda281b38671f8e7215f5b62f558ab9fe72
Ted M. Young <tedyoung@gmail.com>
2021-06-24 10:18:33 -0700

    Added Wallet with tests for isEmpty, addMoney, and balance().

---
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -0,0 +1,29 @@
+package com.jitterted.ebp.blackjack;
+
+public class Wallet {
+
+    private int balance;
+
+    public Wallet() {
+        balance = 0;
+    }
+
+    public boolean isEmpty() {
+        return balance == 0;
+    }
+
+    public void addMoney(int amount) {
+        requireAmountGreaterThanZero(amount);
+        balance += amount;
+    }
+
+    public int balance() {
+        return balance;
+    }
+
+    private void requireAmountGreaterThanZero(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -0,0 +1,74 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WalletTest {
+
+    @Test
+    public void newWalletIsEmpty() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.isEmpty())
+                .isTrue();
+    }
+
+    @Test
+    public void newWalletWhenAddMoneyThenIsNotEmpty() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(10);
+
+        assertThat(wallet.isEmpty())
+                .isFalse();
+    }
+
+    @Test
+    public void newWalletHasZeroBalance() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.balance())
+                .isZero();
+    }
+
+    @Test
+    public void newWalletAdd1BalanceIs1() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(1);
+
+        assertThat(wallet.balance())
+                .isEqualTo(1);
+    }
+
+    @Test
+    public void newWalletAdd7AndAdd8ThenBalanceIs15() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(7);
+        wallet.addMoney(8);
+
+        assertThat(wallet.balance())
+                .isEqualTo(7 + 8);
+    }
+
+    @Test
+    public void addZeroMoneyThrowsException() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThatThrownBy(() -> {
+            wallet.addMoney(0);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void addNegativeMoneyThrowsException() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThatThrownBy(() -> {
+            wallet.addMoney(-1);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
